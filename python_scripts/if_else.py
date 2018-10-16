55217e1078bb3b337142c139fa8fa41b590546ef
mwo <mwo@pfb19.cshl.edu>
2018-10-16 15:46:11 -0400

    added if_else* scripts

---
+++ python_scripts/if_else.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+number = 50
+if number <100 : 
+	print("True")
+else: 
+	print("False")
---
+++ python_scripts/if_else_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+number = int(sys.argv[1])
+if number <100 : 
+        print("True")
+else: 
+        print("False")
