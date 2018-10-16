97fb70a9d7cca3e822b90830157a3fe4dce71337
mwo <mwo@pfb19.cshl.edu>
2018-10-16 11:14:37 -0400

    added name.py

---
+++ name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+names = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name: '+ names ) #added name to print
+print('My favorite color: ', color)
+print('My favorite activity: ', activity)
+print('My favorite animal: ', animal)
