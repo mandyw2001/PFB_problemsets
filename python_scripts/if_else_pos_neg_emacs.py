070fab7a8f55874df123c74dc84fc410918c3510
mwo <mwo@pfb19.cshl.edu>
2018-10-17 11:49:10 -0400

    new scripts added for day 3

---
+++ python_scripts/if_else_pos_neg_emacs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+number = int(sys.argv[1])
+if number >0 :
+    print('Positive')
+    if number < 50:
+        print('The number is less than 50')
+        if number%2==0:
+            print ('It is an even number that is smaller than 50')
+        elif number >50:
+            if number%3==0:
+                print ('It is larger than 50 and divisible by 3')
+elif number ==0 :
+    print ('The number is equal to 0')
+else:
+    print('Negative')
+                
